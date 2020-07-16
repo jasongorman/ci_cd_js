63cabe085830e6fdc2f389f3cddbc51a0bbd9da8
Jason Gorman <jason.gorman@codemanship.com>
2020-07-16 10:10:30 +0100

    Deleted source for new attempt

--- buy.js
+++
@@ -1,9 +0,0 @@
-function buy(cd, amount, card, payments) {
-    if (amount > cd.stock)
-        throw new Error("Out of stock");
-    if (!payments(card, cd.price))
-        throw new Error("Payment rejected");
-    return {stock: cd.stock - amount};
-}
-
-module.exports = buy;
--- search.js
+++
@@ -1,9 +0,0 @@
-function matches(cd, artist, title) {
-    return cd.artist == artist && cd.title == title;
-}
-
-function search(catalogue, artist, title) {
-    return catalogue.find((cd) => matches(cd, artist, title));
-}
-
-module.exports = search;
